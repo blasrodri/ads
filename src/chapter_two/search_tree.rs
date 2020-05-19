fef91dcf8e1f3ba82fb7bd2ac6d84c4a79fd86e2
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-05-19 22:07:27 +0200

    wip

--- src/chapter_two/search_tree.rs
+++ src/chapter_two/search_tree.rs
@@ -57,6 +57,39 @@ impl<K, V> BinaryTree<K, V> {
                 .insert(node);
         }
     }
+
+    #[allow(dead_code)]
+    fn delete(&mut self, node: Node<K, V>)
+    where
+        K: Eq + Clone + PartialOrd,
+        V: Eq + Clone,
+    {
+        if self.root.is_none() {
+            return
+        }
+
+        if node.key == self.root.as_ref().unwrap().key {
+            if self.root.as_ref().unwrap().left.is_none() && self.root.as_ref().unwrap().right.is_none() {
+                self.root = None;
+            } else if self.root.as_ref().unwrap().right.is_some() {
+                // promote this node to root
+                // let recursion handle the rest
+                let right = self
+                    .root
+                    .as_ref()
+                    .unwrap()
+                    .right
+                    .as_ref()
+                    .unwrap()
+                    .borrow()
+                    .clone();
+                self.root.as_mut().unwrap().key =  right.key.clone();
+                self.root.as_mut().unwrap().value =  right.value.clone();
+                self.root.as_mut().unwrap().delete(right);
+            }
+            return;
+        }
+    }
 }
 
 impl<K, V> Node<K, V> {
@@ -84,6 +117,48 @@ impl<K, V> Node<K, V> {
             self.left.as_mut().unwrap().borrow_mut().insert(node);
         }
     }
+
+    #[allow(dead_code)]
+    fn delete(&mut self, node: Node<K, V>)
+    where
+        K: Eq + Clone + PartialOrd,
+        V: Eq + Clone,
+    {
+        // self is the parent of the node we want to delete.
+        // The invariant we rely on is that, in this function
+        // we always have a parent. The is true, because the only case
+        // where this does not hold is when the node to delete is the root.
+        // And that is addressed on the `delete` function within the BinaryTree
+        if self.right.is_none() && self.left.is_none() {
+            // the node does not exist. Thus, nothing to delete.
+            return;
+        }
+        // The node *may* exist
+        if self.key < node.key && self.right.is_some() {
+            let right_key = self.right.as_ref().unwrap().borrow().key.clone();
+            if node.key != right_key {
+                return self.right.as_ref().unwrap().borrow_mut().delete(node);
+            } else {
+                //the child is the one to delete
+                let right_value = self.right.as_ref().unwrap().borrow().key.clone();
+                self.right.as_ref().unwrap().borrow_mut().key =
+            }
+        }
+        if self.key > node.key && self.left.is_some() {
+            let left_key = self.left.as_ref().unwrap().borrow().key.clone();
+            if node.key != left_key {
+                return self.left.as_ref().unwrap().borrow_mut().delete(node);
+            }
+        }
+
+        if self.key == node.key {
+            if self.right.is_some() {
+                self.key = self.right.as_ref().unwrap().borrow().key.clone();
+                self.value = self.right.as_ref().unwrap().borrow().value.clone();
+            }
+        }
+    }
+        // The node does not exist...
 }
 
 #[cfg(test)]
@@ -151,5 +226,41 @@ mod test {
         assert_eq!(fourteen.value, "fourteen");
         assert_eq!(fourteen.left.as_ref().unwrap().borrow().value, "thirteen");
         assert_eq!(fourteen.right.as_ref().unwrap().borrow().value, "fifteen");
+    }
+
+    #[test]
+    fn test_delete() {
+        let root_node = Node {
+            key: 12,
+            value: "twelve",
+            left: None,
+            right: None,
+        };
+
+        let new_node = Node {
+            key: 11,
+            value: "eleven",
+            left: None,
+            right: None,
+        };
+
+        let new_node2 = Node {
+            key: 14,
+            value: "fourteen",
+            left: None,
+            right: None,
+        };
+
+        let mut bt = BinaryTree {
+            root: Some(root_node.clone()),
+        };
+
+        bt.insert(new_node.clone());
+        bt.insert(new_node2.clone());
+        assert_eq!(bt.root.is_some(), true);
+        assert_eq!(bt.root.as_ref().unwrap().value, "twelve");
+        bt.delete(root_node.clone());
+        //bt.delete(new_node.clone());
+        dbg!(&bt);
     }
 }
