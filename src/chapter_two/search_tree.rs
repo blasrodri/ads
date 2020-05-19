96337b8a30a257c473ee03670e427e29839cd24b
Blas Rodriguez Irizar <rodrigblas@gmail.com>
2020-05-19 15:45:56 +0200

    chapter_two: add insert in search tree

--- src/chapter_two/search_tree.rs
+++ src/chapter_two/search_tree.rs
@@ -27,6 +27,41 @@ impl<K, V> BinaryTree<K, V> {
         }
         None
     }
+
+    #[allow(dead_code)]
+    fn insert(&mut self, key: K, value: V)
+    where
+        K: Eq + PartialOrd + std::fmt::Debug,
+        V: Clone + std::fmt::Debug,
+    {
+        if self.key == key {
+            self.element = value;
+        } else if self.key > key {
+            if self.left.is_some() {
+                let mut left = self.left.as_ref().unwrap().borrow_mut();
+                left.insert(key, value);
+            } else {
+                self.left = Some(Rc::new(RefCell::new(BinaryTree {
+                    key,
+                    element: value,
+                    left: None,
+                    right: None,
+                })));
+            }
+        } else if self.key < key {
+            if self.right.is_some() {
+                let mut right = self.right.as_ref().unwrap().borrow_mut();
+                right.insert(key, value);
+            } else {
+                self.right = Some(Rc::new(RefCell::new(BinaryTree {
+                    key,
+                    element: value,
+                    left: None,
+                    right: None,
+                })));
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +99,24 @@ mod test {
         };
         assert_eq!(tree.find(10), Some("asd"));
         assert_eq!(tree.find(4), Some("askl"));
+    }
+
+    #[test]
+    fn test_insert_key() {
+        let mut tree = BinaryTree {
+            key: 10,
+            element: "asd",
+            left: Some(Rc::new(RefCell::new(BinaryTree {
+                key: 4,
+                element: "askl",
+                left: None,
+                right: None,
+            }))),
+            right: None,
+        };
+        tree.insert(11, "1a1");
+        tree.insert(0, "0a");
+        assert_eq!(tree.find(11), Some("1a1"));
+        assert_eq!(tree.find(0), Some("0a"));
     }
 }
